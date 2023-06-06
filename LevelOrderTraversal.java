5091d114c60411401cd0f062128d3d4ee8b1b7a6
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-06 18:19:12 +0530

    Completed Level Order Traversal

---
+++ LevelOrderTraversal.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
+public class LevelOrderTraversal {
+    public void traverse(BinaryTree.TreeNode root){
+        Queue<BinaryTree.TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        System.out.print(root.data+" ");
+        while(!queue.isEmpty()){
+            BinaryTree.TreeNode pointer = queue.poll();
+            if(pointer.left!=null){
+                queue.offer(pointer.left);
+                System.out.print(pointer.left.data+" ");
+            }
+            if(pointer.right!=null){
+                queue.offer(pointer.right);
+                System.out.print(pointer.right.data+" ");
+            }
+        }
+    }
+}
