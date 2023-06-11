e1506a9e6e4c3dc3e41fabbffe2b9b2acac3c18c
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-11 22:36:27 +0530

    To be changed the implementation seems not correct

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,69 +1,72 @@
 import java.util.LinkedList;
-import java.util.Scanner;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class BinarySearchTree {
-    node root;
-    node pointer;
-    class node{
-        node left;
-        node right;
-        Object data;
-        public node(Object data){
+    Node root;
+
+    class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
             this.data = data;
-            
         }
     }
-    public node insert(node root, int data){
-        node new_node =  new node(data);
-        if(root == null){
-            root = new_node;
-            return root;
+
+    public Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
         }
-        if(data<(int)root.data){
+
+        if (data < root.data) {
             root.left = insert(root.left, data);
+        } else if (data > root.data) {
+            root.right = insert(root.right, data);
         }
-        if(data>(int)root.data){
-            root.right = insert(root.right,data);
-        }
+
         return root;
     }
 
-    public void insert(int data){
+    public void insert(int data) {
         root = insert(root, data);
-
     }
 
-    public void level_order(node root){
-        if(root == null){
+    public void level_order(Node root) {
+        if (root == null) {
             return;
         }
-        Queue<node> queue = new LinkedList<>();
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            node pointer = queue.poll();
-            System.out.print(pointer.data);
-            if(pointer.left!=null){
-                queue.add(pointer.left);
-            }
-            if(pointer.right!=null){
-                queue.add(pointer.right);
-            }
-        }
 
-        
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                Node current = queue.poll();
+                System.out.print(current.data + " ");
+
+                if (current.left != null) {
+                    queue.add(current.left);
+                }
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+            System.out.println();
+        }
     }
 
-    public static void main(String []args){
-        BinarySearchTree bSearchTree = new BinarySearchTree();
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        for(int i=0; i<n;i++){
+        for (int i = 0; i < n; i++) {
             int value = scanner.nextInt();
-            bSearchTree.insert(value);
+            bst.insert(value);
         }
-        bSearchTree.level_order(bSearchTree.root);
+        bst.level_order(bst.root);
         scanner.close();
     }
 }
-
